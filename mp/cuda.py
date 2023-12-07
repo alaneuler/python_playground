bbc16e298a4bf131fcce2dacfc92111e054ab649
alaneuler <alaneuler@qq.com>
2023-12-07 15:32:27 +0800

    format mp

--- mp/cuda.py
+++ mp/cuda.py
@@ -1,15 +1,19 @@
-import torch
 from multiprocessing import Process, set_start_method
 
+import torch
+
 input = torch.tensor([[1.0]], device="cuda")
+
 
 def process(i, name):
     print(f"In {name} {i}...")
     input = torch.tensor([[1.0]], device=f"cuda:{i}")
     print(input)
 
+
 if __name__ == "__main__":
     # set_start_method("spawn")
+    set_start_method("fork")
     processes = [Process(target=process, args=(i, "test")) for i in range(3)]
     for p in processes:
         p.start()
--- mp/main.py
+++ mp/main.py
@@ -1,15 +1,16 @@
 import os
 from multiprocessing import Process, set_start_method
 
+
 def foo(i):
     print(f"Process {os.getpid()} in {i}...")
     print(f"Exiting {i}...")
 
+
 if __name__ == "__main__":
     print(f"Process {os.getpid()}")
     set_start_method("spawn")
-    processes = [Process(target=foo, args=(i, ))
-                 for i in range(3)]
+    processes = [Process(target=foo, args=(i,)) for i in range(3)]
     for p in processes:
         p.start()
     for p in processes:
