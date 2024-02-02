08ee6437430e5517af62cb074ca850d111f4f940
alaneuler <alaneuler@qq.com>
2024-02-02 11:44:17 +0800

    bert classifier

--- basic/parameters.py
+++ basic/parameters.py
@@ -1,6 +1,7 @@
 def f(*, arg1, arg2):
     print(arg1, arg2)
 
+
 # This one won't work.
 # f(1, 2)
 f(arg1=1, arg2=2)
---
+++ bert_classifier/model.py
@@ -0,0 +1,52 @@
+from datasets import load_dataset
+from transformers import (
+    BertForSequenceClassification,
+    BertTokenizer,
+    Trainer,
+    TrainingArguments,
+)
+
+model_path = "/data2/public_file/LLM_model/chinese-roberta-wwm-ext"
+max_length = 5
+
+
+def tokenize_map(examples):
+    return tokenizer(
+        examples["text"],
+        max_length=max_length,
+        padding="max_length",
+        truncation=True,
+    )
+
+
+tokenizer = BertTokenizer.from_pretrained(model_path)
+data_set = load_dataset(
+    "json",
+    data_files={"train": "./data/train.json", "validation": "./data/val.json"},
+)
+data_set = data_set.map(tokenize_map, batched=True)
+
+model = BertForSequenceClassification.from_pretrained(
+    model_path,
+    num_labels=2,
+)
+model.train()
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    num_train_epochs=5,
+    logging_dir="./logs",
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    save_total_limit=2,
+)
+trainer = Trainer(
+    model,
+    args=training_args,
+    train_dataset=data_set["train"],
+    eval_dataset=data_set["validation"],
+)
+
+trainer.train()
+# trainer.save_model("./results")
